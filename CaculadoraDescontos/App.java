06293e97f805fbf24578cf25381a3286178347b5
coutinhogabriel <gabriel.coutin@hotmail.com>
2023-02-13 21:58:47 -0300

    13/02/2023

--- CaculadoraDescontos/App.java
+++ CaculadoraDescontos/App.java
@@ -2,6 +2,16 @@ package CaculadoraDescontos;
 
 public class App {
     public static void main(String[] args) {
-        
-    }
+        CalculadoraDescontos objeto1 = new CalculadoraDescontos();
+        objeto1.calcular10();
+        objeto1.calcular20();
+        objeto1.calcular30();
+        objeto1.calcular40();
+        objeto1.calcular50();
+        objeto1.calcular60();
+        objeto1.calcular70();
+        objeto1.calcular80();
+        objeto1.calcular90();
+
+}
 }
--- CaculadoraDescontos/CalculadoraDescontos.java
+++ CaculadoraDescontos/CalculadoraDescontos.java
@@ -1,6 +1,63 @@
 package CaculadoraDescontos;
 
+import java.util.Scanner;
+
 public class CalculadoraDescontos {
-    
-    
+
+    public void calcular10 (){
+        Scanner sc = new Scanner (System.in);
+        double precoProduto = sc.nextDouble();
+        final int desconto10 = 10;
+    double precoComDesconto = precoProduto - (precoProduto*desconto10/100); 
+    System.out.println("Informe o Valor do produto" + precoComDesconto);
+    sc.close();
+}
+    public void calcular20 (){double precoProduto;
+        precoProduto=200;
+        final int desconto20 = 20;
+    double precoComDesconto = precoProduto - (precoProduto*desconto20/100); 
+System.out.println("O Preço com desconto é R$" + precoComDesconto);
+}
+public void calcular30 (){double precoProduto;
+    precoProduto=200;
+    final int desconto30 = 30;
+double precoComDesconto = precoProduto - (precoProduto*desconto30/100); 
+System.out.println("O Preço com desconto é R$" + precoComDesconto);
+}
+public void calcular40 (){double precoProduto;
+    precoProduto=200;
+    final int desconto40 = 40;
+double precoComDesconto = precoProduto - (precoProduto*desconto40/100); 
+System.out.println("O Preço com desconto é R$" + precoComDesconto);
+}
+public void calcular50 (){double precoProduto;
+    precoProduto=200;
+    final int desconto50 = 50;
+double precoComDesconto = precoProduto - (precoProduto*desconto50/100); 
+System.out.println("O Preço com desconto é R$" + precoComDesconto);
+}
+public void calcular60 (){double precoProduto;
+    precoProduto=200;
+    final int desconto60 = 60;
+double precoComDesconto = precoProduto - (precoProduto*desconto60/100); 
+System.out.println("O Preço com desconto é " + precoComDesconto+ " R$");
+}
+public void calcular70 (){double precoProduto;
+    precoProduto=200;
+    final int desconto70 = 70;
+double precoComDesconto = precoProduto - (precoProduto*desconto70/100); 
+System.out.println("O Preço com desconto é " + precoComDesconto+ " R$");
+}
+public void calcular80 (){double precoProduto;
+    precoProduto=200;
+    final int desconto80 = 80;
+double precoComDesconto = precoProduto - (precoProduto*desconto80/100); 
+System.out.println("O Preço com desconto é " + precoComDesconto+ " R$");
+}
+public void calcular90 (){double precoProduto;
+    precoProduto=200;
+    final int desconto90 = 90;
+double precoComDesconto = precoProduto - (precoProduto*desconto90/100); 
+System.out.println("O Preço com desconto é " + precoComDesconto+ " R$");
+}
 }
